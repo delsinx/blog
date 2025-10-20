e4f9c242abd0dfafb639e3f88233c6a5bfaa1069
Delson Filho <delson_filho@hotmail.com.br>
2025-10-20 17:58:59 -0300

    feat: created and configurated DTO and Mapper

--- src/main/java/com/example/demo/Mapper/BlogMapper.java
+++ src/main/java/com/example/demo/Mapper/BlogMapper.java
@@ -1,4 +1,32 @@
 package com.example.demo.Mapper;
 
+import com.example.demo.Model.BlogModel;
+import com.example.demo.User.DTO.BlogDTO;
+import org.springframework.stereotype.Component;
+
+@Component
 public class BlogMapper {
+    public BlogModel map(BlogDTO blogDTO){
+        BlogModel blogModel = new BlogModel();
+
+       blogModel.setId(blogDTO.getId());
+       blogModel.setTitle(blogDTO.getTitle());
+       blogModel.setContent(blogDTO.getContent());
+       blogModel.setAuthor(blogDTO.getAuthor());
+       blogModel.setUpdateAt(blogDTO.getUpdateAt());
+       blogModel.setCreateAt(blogDTO.getCreateAt());
+        return blogModel;
+    }
+
+    public BlogDTO map(BlogModel blogModel){
+        BlogDTO blogDTO = new BlogDTO();
+        blogDTO.setId(blogModel.getId());
+        blogDTO.setTitle(blogModel.getTitle());
+        blogDTO.setContent(blogModel.getContent());
+        blogDTO.setAuthor(blogModel.getAuthor());
+        blogDTO.setUpdateAt(blogModel.getUpdateAt());
+        blogDTO.setCreateAt(blogModel.getCreateAt());
+        return blogDTO;
+    }
+
 }
--- src/main/java/com/example/demo/User/DTO/BlogDTO.java
+++ src/main/java/com/example/demo/User/DTO/BlogDTO.java
@@ -1,4 +1,22 @@
 package com.example.demo.User.DTO;
 
+import com.example.demo.Model.UserModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BlogDTO {
+    private Long id;
+    private String title;
+    private String content;
+    private UserModel author;
+    private LocalDateTime createAt = LocalDateTime.now();
+    private LocalDateTime updateAt;
+
+
 }
